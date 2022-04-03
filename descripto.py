59a2328104d0d9420aff1e86dc9719f79bc89fe7
Maxwell Bastos <102771260+MaxwellBastos@users.noreply.github.com>
2022-04-02 22:15:01 -0300

    Update descripto.py

--- descripto.py
+++ descripto.py
@@ -1,5 +1,9 @@
 import os
 import pyaes
+from time import sleep
+sleep(1)
+print('Iniciando processo de descriptografia...')
+sleep(2)
 
 fileName = 'olhospy.jpg.pyransom'
 file = open(fileName, 'rb')
